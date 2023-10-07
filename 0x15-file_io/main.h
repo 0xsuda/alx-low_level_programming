805ef42d655174ab60bf0850003f845a70c28b86
0xsuda <osama.suliman@protonmail.com>
2023-10-06 20:01:38 -0700

    io

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -4,8 +4,10 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
-int append_text_to_file(const char *filename, char *text_content);
+
 ssize_t read_f(const char *filename, size_t letters);
 int c_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+
 #endif
 
